8e6f9e7ab11f5fcfe67ea5f02e07bb7d91ebdcb7
zeroequalsone <real.rizzle74@gmail.com>
2026-04-12 02:03:27 +0200

    Set date of Nasa APOD to Date object

--- app/api/nasa/route.ts
+++ app/api/nasa/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server"
 
 export async function GET() {
-    const res = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_APOD_API_KEY}&date=2026-04-04`)
+    const res = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_APOD_API_KEY}&date=${new Date().toISOString().split('T')[0]}`)
     const data = await res.json()
 
     return NextResponse.json(data)

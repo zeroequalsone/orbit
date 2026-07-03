bf980a13c32b7a1a8a43d75aa2ed811273010ee6
zeroequalsone <real.rizzle74@gmail.com>
2026-07-03 21:48:03 +0200

    Removed googleMapsUrl lib file

--- app/data/lib/googleMapsUrl.ts
+++
@@ -1,5 +0,0 @@
-export const googleMapsUrl = (coordinates: string) => {
-  const url = new URL("https://www.google.com/maps");
-  url.searchParams.set("q", coordinates);
-  return url.toString();
-};

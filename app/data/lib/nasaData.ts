9fceac150f1a4756f762e82ba621d248b2eeaa78
zero <98187128+zeroequalsone@users.noreply.github.com>
2026-07-04 10:19:30 +0200

    Refactor: Project Structure Nasa Data

---
+++ app/data/lib/nasaData.ts
@@ -0,0 +1,51 @@
+export async function getNasaData() {
+  const apikey = process.env.NASA_APOD_API_KEY;
+
+  // ERROR HANDLING
+  if (!apikey) {
+    throw new Error(
+      "NASA_APOD_API_KEY ist nicht in den Umgebungsvariablen definiert.",
+    );
+  }
+
+  const today = new Date().toLocaleDateString("en-CA", {
+    timeZone: "America/New_York",
+  });
+
+  const res = await fetch(
+    `https://api.nasa.gov/planetary/apod?api_key=${apikey}&date=${today}`,
+    { next: { revalidate: 3600 } },
+  );
+
+  // ERROR HANDLING
+  if (!res.ok) {
+    switch (res.status) {
+      case 400:
+        throw new Error(
+          "Ungültige Anfrage (400) - diese Koordinaten existieren (noch) nicht im Universum.",
+        );
+      case 401:
+        throw new Error(
+          "Zugriff verweigert (401) - ohne gültige Berechtigung kein Zugang zum Kontrollzentrum.",
+        );
+      case 403:
+        throw new Error(
+          "Keine Berechtigung (403) - diese Sternenregion ist für deine Mission nicht freigegeben.",
+        );
+      case 404:
+        throw new Error(
+          "Nicht gefunden (404) - dieses Himmelsobjekt konnte im kosmischen Netz nicht lokalisiert werden.",
+        );
+      case 500:
+        throw new Error(
+          "Bordcomputer-Fehler (500) - ein unerwarteter Störimpuls hat das System beeinträchtigt.",
+        );
+      default:
+        throw new Error(
+          "Unbekannter Fehler - das Signal wurde von einem schwarzen Loch verschluckt.",
+        );
+    }
+  }
+
+  return res.json();
+}

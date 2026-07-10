4b5cd5d27a984c5b42e52cb62f4385a84074b348
zeroequalsone <real.rizzle74@gmail.com>
2026-07-10 18:41:13 +0200

    refactor(planetSelector): refactored css and made responsive

--- app/components/solar-system/PlanetSelector.tsx
+++ app/components/solar-system/PlanetSelector.tsx
@@ -9,13 +9,13 @@ export default function PlanetSelector({
   setPlanetId: Dispatch<number>;
 }) {
   return (
-    <section className="min-h-screen flex justify-center items-center flex-col">
-      <div className="flex flex-col">
-        <div className="flex gap-18">
+    <section className="min-h-screen flex justify-center items-center">
+      <div className="flex flex-col justify-center p-8 max-w-384 w-full gap-10">
+        <div className="flex lg:flex-row flex-col justify-between">
           {[...planets].reverse().map((planet) => (
             <div
               key={planet.id}
-              className={`flex flex-col items-center justify-between cursor-pointer ${planetId === planet.id ? "text-white" : "text-neutral-400"}`}
+              className={`flex lg:flex-col items-center justify-between cursor-pointer ${planetId === planet.id ? "text-white" : "text-neutral-400"}`}
               onClick={() => setPlanetId(planet.id)}
             >
               <p>
@@ -25,14 +25,17 @@ export default function PlanetSelector({
                 AE
               </p>
               <div
-                className={`rounded-full mb-8 mt-8`}
+                className={`hidden lg:flex rounded-full mb-8 mt-8 bg-center bg-no-repeat bg-size-[100%]`}
                 style={{
                   height: `${planet.id === 6 ? Math.sqrt(planet.diameter / 5) : Math.sqrt(planet.diameter / 20)}px`,
                   width: `${planet.id === 6 ? Math.sqrt(planet.diameter / 5) : Math.sqrt(planet.diameter / 20)}px`,
                   backgroundImage: `url('${planet.iconUrl}')`,
-                  backgroundSize: "100%",
-                  backgroundPosition: "center",
-                  backgroundRepeat: "no-repeat",
+                }}
+              />
+              <div
+                className={`lg:hidden rounded-full mb-8 mt-8 bg-center bg-no-repeat bg-size-[100%] size-12`}
+                style={{
+                  backgroundImage: `url('${planet.iconUrl}')`,
                 }}
               />
               <div className="flex flex-col gap-8 items-center">
@@ -42,7 +45,7 @@ export default function PlanetSelector({
             </div>
           ))}
         </div>
-        <p className="text-sm mt-28 text-neutral-400">
+        <p className="text-sm text-neutral-400">
           AE (Astronomische Einheit) — die Distanz der Erde zur Sonne.
         </p>
       </div>

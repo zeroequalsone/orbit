f3ba7b3622356ae15d03e200a741709a5da794a9
zero <98187128+zeroequalsone@users.noreply.github.com>
2026-07-15 02:25:48 +0200

    refactor(tools-page): extract tool button and improve layout (#57)

--- app/tools/page.tsx
+++ app/tools/page.tsx
@@ -1,35 +1,21 @@
-import Link from "next/link";
+import ToolButton from "@/components/tools/ToolButton";
+
+const tools = [
+  { name: "Distanz-Rechner", href: "distance-calculator" },
+  { name: "Gewicht-Rechner", href: "weight-calculator" },
+  { name: "Größen-Rechner", href: "size-calculator" },
+  { name: "Landsat Name Generator", href: "landsat-name-generator" },
+];
 
 export default function Tools() {
   return (
     <div className="flex justify-center items-center min-h-screen">
-      <div className="flex justify-center items-center flex-col gap-10 w-4/5">
+      <div className="flex items-center flex-col p-8 mt-20 gap-10 lg:max-w-384">
         <h1 className="text-5xl font-bold uppercase tracking-[0.3em]">Tools</h1>
-        <div className="flex gap-10">
-          <Link
-            href="/tools/distance-calculator"
-            className="border-2 border-white rounded-full px-8 py-4 text-xl font-light"
-          >
-            Distanz-Rechner
-          </Link>
-          <Link
-            href="/tools/weight-calculator"
-            className="border-2 border-white rounded-full px-8 py-4 text-xl font-light"
-          >
-            Gewicht-Rechner
-          </Link>
-          <Link
-            href="/tools/size-calculator"
-            className="border-2 border-white rounded-full px-8 py-4 text-xl font-light"
-          >
-            Größen-Rechner
-          </Link>
-          <Link
-            href="/tools/landsat-name-generator"
-            className="border-2 border-white rounded-full px-8 py-4 text-xl font-light"
-          >
-            Landsat Name Generator
-          </Link>
+        <div className="grid lg:grid-cols-2 gap-10 text-center">
+          {tools.map((tools) => (
+            <ToolButton key={tools.name} href={tools.href} name={tools.name} />
+          ))}
         </div>
       </div>
     </div>
